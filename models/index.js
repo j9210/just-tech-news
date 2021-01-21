70464d512e81adc8956daa650016a85f6fc2d9a1
j9210 <71297542+j9210@users.noreply.github.com>
2021-01-21 14:58:43 -0600

    create vote model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,36 @@
+const Post = require('./Post');
+const { belongsToMany } = require('./User');
 const User = require('./User');
+const Vote = require('./Vote');
 
-module.exports = { User };
+// associations
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
 // this file imports the user model and exports and object with it as a property
