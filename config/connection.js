712c68480db2ab80b5be4e07d6f5cf2fe60a77fc
j9210 <71297542+j9210@users.noreply.github.com>
2021-01-21 12:29:21 -0600

    dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // import the sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // create connection to our database, pass in your mysql username and pw
-const sequelize = new Sequelize('just_tech_news_db', 'juliaolivia.t@gmail.com', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
