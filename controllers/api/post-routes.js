b7f34ea0e336c4e900748b14804919289a359d5e
j9210 <71297542+j9210@users.noreply.github.com>
2021-01-27 15:24:28 -0600

    login

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -29,12 +29,7 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData =>{
-      // pass a single object into the homepage template
-      console.log(dbPostData[0]);
-      const posts = dbPostData.map(post => post.get ({ plain: true }));
-      res.render('homepage', { posts });
-    })
+    .then(dbPostData => res.json(dbPostData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -96,13 +91,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/:id', (req, res) => {
